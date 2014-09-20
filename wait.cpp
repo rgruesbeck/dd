d51fe826ce59710a9aa27b30e24c07fe401258c8
Ron Gruesbeck <rongruesbeck@gmail.com>
2014-09-20 04:35:02 -0700

    add crapy count down

--- wait.cpp
+++ wait.cpp
@@ -1,8 +1,14 @@
+#include <iostream>
 #include <chrono>
 #include <thread>
 using namespace std;
 
 void wait(int n){
-  int mins = n * 1000 * 60;
-  std::this_thread::sleep_for(std::chrono::milliseconds(mins));
+  int count = 0;
+  int sec = n * 60;
+  while (count < sec){
+    cout << count << endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    count += 1;
+  }
 }
